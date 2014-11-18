6b45588f778a5abad6fb2c5ef2548ea4af8fe2e3
Sam Sinai <samsinai@Sams-MacBook-Pro.local>
2014-11-18 14:57:29 -0500

    scrape removed

--- arxiv_scrape.py
+++
@@ -1,5 +0,0 @@
-from arxiv_scraper import *
-import sys
-categories=['hep-th','astro-ph']
-output,query=scrape_arxiv_api_convert_to_dict(categories[1],int(sys.argv[1]),int(sys.argv[2]))
-save_into_pickle(output,query)
